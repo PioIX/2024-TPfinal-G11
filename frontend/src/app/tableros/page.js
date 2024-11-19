2a4e9807c687ad7e0690426de9fa69b1668e5341
felipe <rarakakiaguero@pioix.edu.ar>
2024-11-19 10:06:15 -0300

    boton para atras

--- frontend/src/app/tableros/page.js
+++ frontend/src/app/tableros/page.js
@@ -2,6 +2,9 @@
 import { useState, useEffect } from 'react';
 import styles from "../styles/Tableros.module.css";
 
+const Atras = () => {
+  router.push('../home');
+};
 function Boards() {
   const [groupedPins, setGroupedPins] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -11,13 +14,13 @@ function Boards() {
     const fetchBoards = async () => {
       try {
         const response = await fetch('http://localhost:4000/boards', {
-            method: 'GET',
-          });
+          method: 'GET',
+        });
         if (!response.ok) {
           throw new Error('Error al cargar los tableros');
         }
         const data = await response.json();
-        setGroupedPins(data); 
+        setGroupedPins(data);
         setLoading(false);
       } catch (err) {
         setError(err.message);
@@ -38,25 +41,28 @@ function Boards() {
 
   return (
     <div className={styles.boardsContainer}>
-    {groupedPins.map((categoryData) => (
-      <div key={categoryData.category} className={styles.categorySection}>
-        <h2 className={styles.categoryTitle}>{categoryData.category}</h2>
-        <div className={styles.pinContainer}>
-          {categoryData.pins.map((pin) => (
-            <div key={pin.id} className={styles.pin}>
-              <img
-                src={pin.image_url}
-                alt={pin.title}
-                className={styles.pinImage}
-              />
-              <h3 className={styles.pinTitle}>{pin.title}</h3>
-              <p className={styles.pinDescription}>{pin.description}</p>
-            </div>
-        ))}
-      </div>
+      <a href="../home">
+      <img className={styles["icono-volver"]} src="flecha_izquierda.png" alt="Volver atras" />
+      </a>
+      {groupedPins.map((categoryData) => (
+        <div key={categoryData.category} className={styles.categorySection}>
+          <h2 className={styles.categoryTitle}>{categoryData.category}</h2>
+          <div className={styles.pinContainer}>
+            {categoryData.pins.map((pin) => (
+              <div key={pin.id} className={styles.pin}>
+                <img
+                  src={pin.image_url}
+                  alt={pin.title}
+                  className={styles.pinImage}
+                />
+                <h3 className={styles.pinTitle}>{pin.title}</h3>
+                <p className={styles.pinDescription}>{pin.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
 
   );
 }
